a9425e550d3adef9b61329523e05d4dcb33cd579
wanglele <wangleping16@163.com>
2018-07-17 19:04:26 +0800

    chapter3

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <m-header></m-header>
+    <tab></tab>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+import MHeader from 'components/m-header/m-header'
+import Tab from 'components/tab/tab'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    MHeader,
+    Tab
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
-  @import "~common/stylus/variable"
 
-    #app
-      color: $color-theme
 </style>
